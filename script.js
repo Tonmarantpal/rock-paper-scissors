a92db76265ff45fe393bd636a16a85dcb0b36944
tonmarantpal <151752778+Tonmarantpal@users.noreply.github.com>
2023-11-30 07:52:53 +0200

    Create round playing function

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length);
     return choice = choices[choice];
 }
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let userChoice = prompt("What will you choose?")
+    userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1)
+    if (computerChoice === userChoice) {
+        return "It's a tie!";
+    } else if (userChoice === "Rock" && computerChoice === "Scissors" || userChoice === "Paper" && computerChoice === "Rock" || userChoice === "Scissors" && computerChoice === "Paper") {
+        return `You win! ${userChoice} beats ${computerChoice}!`
+    } else {
+        return `You lose! ${computerChoice} beats ${userChoice}!`
+    }
+}
