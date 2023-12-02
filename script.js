b1abb08736a843909b9f21e9d1be25b88340aea8
tonmarantpal <151752778+Tonmarantpal@users.noreply.github.com>
2023-12-02 10:50:19 +0200

    Finish UI for RPS

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 const choices = ["Rock", "Paper", "Scissors"]
+const buttons = document.querySelectorAll("button")
+const p = document.querySelector(".result")
+const score = document.querySelector(".score")
+p.textContent = "Please begin!"
+let playerScore = 0;
+let computerScore = 0;
+score.textContent = `Player's score: ${playerScore} | Computer's score :${computerScore}`
+
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * choices.length);
     return choice = choices[choice];
 }
 
-function playRound() {
+function playRound(userChoice) {
     let computerChoice = getComputerChoice();
-    let userChoice = prompt("What will you choose?")
-    userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1)
+    userChoice = userChoice
     if (computerChoice === userChoice) {
-        return "It's a tie!";
+        score.textContent = `Player's score: ${playerScore} | Computer's score :${computerScore}`
+        p.textContent = "It's a tie!";
     } else if (userChoice === "Rock" && computerChoice === "Scissors" || userChoice === "Paper" && computerChoice === "Rock" || userChoice === "Scissors" && computerChoice === "Paper") {
-        return `You win! ${userChoice} beats ${computerChoice}!`
+        playerScore++
+        score.textContent = `Player's score: ${playerScore} | Computer's score :${computerScore}`
+        p.textContent = `You win! ${userChoice} beats ${computerChoice}!`
     } else {
-        return `You lose! ${computerChoice} beats ${userChoice}!`
+        computerScore++
+        score.textContent = `Player's score: ${playerScore} | Computer's score :${computerScore}`
+        p.textContent = `You lose! ${computerChoice} beats ${userChoice}!`
+    }
+
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore > computerScore) {
+            p.textContent = "GAME OVER! You are the winner!"
+            playerScore = 0;
+            computerScore = 0;
+        } else {
+            p.textContent = "GAME OVER! Computer is the winner!"
+            playerScore = 0;
+            computerScore = 0;
+        }
     }
 }
 
-function game(n) {
-    for (i = 0; i < n; i++) {
-        console.log(playRound())
-    }
-}
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        playRound(e.target.innerText)
+    } )
+})
+
